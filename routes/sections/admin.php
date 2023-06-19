ff98765fa59ea9586698be28a2bf6884375037de
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-06-19 11:36:13 +0330

    feat: added user CRUD

--- routes/sections/admin.php
+++ routes/sections/admin.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard',function(){
+Route::get('/dashboard', function () {
     die('we lack view here');
 })->name('dashboard');
+
+Route::resource('users', UserController::class);
