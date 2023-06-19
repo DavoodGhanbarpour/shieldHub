de35149f7b9b9ac9e7ee8ae735ab27a5684229a1
mahdi <m.mosayyebi@tecteam.ir>
2023-06-20 00:51:14 +0330

    layouts and pages | users page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
+        return view('admin.pages.user.users');
         return UserResource::collection(User::all());
     }
 
--- routes/sections/admin.php
+++ routes/sections/admin.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    exit('we lack view here');
+Route::get('dashboard', function () {
+    return view('admin.pages.home.home');
 })->name('dashboard');
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class, [
+    'names' => [
+        'index' => 'users',
+        'create' => 'userAdd',
+        'edit' => 'userEdit',
+    ]
+]);
