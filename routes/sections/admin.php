c8cc4ed68362880f57334aa9cb39c16c5414b455
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-06-20 23:13:34 +0330

    refactor: better naming

--- routes/sections/admin.php
+++ routes/sections/admin.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', function () {
     return view('admin.pages.home.home');
-})->name('dashboard');
+})->name('home');
 
 Route::resource('users', UserController::class);
