3e76fab1ebcb802f682c26c1a98e27af5ed3422a
mahdi <m.mosayyebi@tecteam.ir>
2023-06-22 17:59:23 +0330

    new profile avatar

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -38,14 +38,14 @@
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                    aria-label="Open user menu">
-                    <span class="avatar avatar-sm" style="background-image: url({{asset('static/avatars/profile_white.png')}})"></span>
+                    <span class="avatar avatar-sm" style="background-image: url({{asset('static/avatars/profile_white_outlined.png')}})"></span>
                     <div class="d-none d-xl-block ps-2">
                         <div>{{auth()->user()->name}}</div>
                         <div class="mt-1 small text-muted">{{ucfirst(auth()->user()->role)}}</div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="./profile.html" class="dropdown-item">Profile</a>
+                    {{-- <a href="./profile.html" class="dropdown-item">Profile</a> --}}
                     <a href="{{ route('auth.logout') }}" class="dropdown-item">Logout</a>
                 </div>
             </div>
