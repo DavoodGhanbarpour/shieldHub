c6d5a647978dd2f439b53e135e9a300dd9c48e1a
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-06-23 18:25:37 +0330

    refactor: applying lang

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -9,7 +9,7 @@
         </h1>
         <div class="navbar-nav flex-row order-md-last">
             <div class="d-none d-md-flex">
-                <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Enable dark mode"
+                <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="{{__('app.dashboard.enable_dark_mode')}}"
                    data-bs-toggle="tooltip" data-bs-placement="bottom">
                     <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
@@ -19,7 +19,7 @@
                         <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
                     </svg>
                 </a>
-                <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="Enable light mode"
+                <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="{{__('app.dashboard.enable_light_mode')}}"
                    data-bs-toggle="tooltip" data-bs-placement="bottom">
                     <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
