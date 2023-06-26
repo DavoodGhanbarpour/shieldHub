9d9444ca5da7c9475948789d274d4069c22b9f33
mahdi <m.mosayyebi@tecteam.ir>
2023-06-27 02:49:45 +0330

    fixed

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -21,7 +21,6 @@
     <link href="{{ asset('css/tabler-vendors.min.css?1684106062') }}" rel="stylesheet"/>
     <link href="{{ asset('css/demo.min.css?1684106062') }}" rel="stylesheet"/>
     <link href="{{ asset('css/custom-css.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('libs/datatables/datatables.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('libs/select2/select2.css') }}" rel="stylesheet"/>
     @stack('styles')
--- resources/views/components/locale/main-styles.blade.php
+++ resources/views/components/locale/main-styles.blade.php
@@ -1,5 +1,5 @@
 @if (App\Models\User::SUPPORTED_LANGUAGES[config()->get('app.locale')]['dir'] == 'ltr')
-    <link href="{{ asset('css/tabler.min.css?1684106062') }}" rel="stylesheet"/>        
+    <link href="{{ asset('css/tabler.css?1684106062') }}" rel="stylesheet"/>        
 @else
     <link href="{{ asset('css/tabler.rtl.css') }}" rel="stylesheet"/>        
 @endif
