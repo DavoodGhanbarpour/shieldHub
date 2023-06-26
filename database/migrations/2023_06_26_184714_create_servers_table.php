b262a346e548ae58558389b76d5db323a7f47bb7
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-06-26 23:34:03 +0330

    fix: ip added

--- database/migrations/2023_06_26_184714_create_servers_table.php
+++ database/migrations/2023_06_26_184714_create_servers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('servers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->ipAddress('ip');
             $table->date('start_date');
             $table->date('end_date');
             $table->text('description')->nullable();
