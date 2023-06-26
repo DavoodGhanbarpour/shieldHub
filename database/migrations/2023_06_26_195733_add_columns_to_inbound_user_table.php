7917d18bcd45fb2375b419301863ea725511c69d
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-06-26 20:02:28 +0330

    feat: added invoice and payments to inbound_user table

---
+++ database/migrations/2023_06_26_195733_add_columns_to_inbound_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('inbound_user', function (Blueprint $table) {
+            $table->unsignedDouble('subscription_price_per_month');
+            $table->text('description');
+            $table->text('private_description');
+            $table->unsignedDouble('payment_amount');
+            $table->date('payment_date');
+            $table->date('start_date');
+            $table->date('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('inbound_user', function (Blueprint $table) {
+            $table->dropColumn('subscription_price_per_month');
+            $table->dropColumn('description');
+            $table->dropColumn('private_description');
+            $table->dropColumn('payment_amount');
+            $table->dropColumn('payment_date');
+            $table->dropColumn('start_date');
+            $table->dropColumn('end_date');
+        });
+    }
+};
