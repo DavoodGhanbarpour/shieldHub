65f4739b98f0c482bffbafd74dda8d7a242681b0
mahdi <m.mosayyebi@tecteam.ir>
2023-06-29 18:07:58 +0330

    style changed

--- resources/views/components/buttons/add.blade.php
+++ resources/views/components/buttons/add.blade.php
@@ -3,7 +3,7 @@
     $title = isset($title) ? $title : __("app.pageComponents.add");
     $link = isset($link) ? $link : route("admin.$lastSegment.create")
 @endphp
-<a href="{{$link}}" class="btn btn-primary d-none d-sm-inline-block">
+<a href="{{$link}}" class="btn btn-success d-none d-sm-inline-block">
     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
          stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
