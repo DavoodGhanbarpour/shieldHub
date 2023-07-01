0a3e91a315d216102481874f1f1e15deba8973c1
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-07-01 20:44:20 +0330

    fix: better language structure

--- resources/views/customer/pages/home/index.blade.php
+++ resources/views/customer/pages/home/index.blade.php
@@ -25,7 +25,7 @@
                                 </td>
                                 <td class="sort-quota">
                                     {{
-                                        __('app.inbounds.days_remain',['count' =>
+                                        __('app.general.days_remain',['count' =>
                                             \Carbon\Carbon::parse($eachInbound->date)->diffInDays(\Carbon\Carbon::now())
                                         ])
                                     }}
@@ -69,7 +69,7 @@
                                         <div class="col-auto">
                                         <span class="bg-green text-white avatar">
                                             <svg xmlns="http://www.w3.org/2000/svg"
-                                                 class="icon icon-tabler icon-tabler-brand-{{__('app.platform.android')}}"
+                                                 class="icon icon-tabler icon-tabler-brand-{{__('app.platforms.android')}}"
                                                  width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                                                  stroke="currentColor" fill="none" stroke-linecap="round"
                                                  stroke-linejoin="round">
