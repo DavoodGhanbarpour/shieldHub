c7435b4495ef81d058c2e6c46634eff3232b19dd
mahdi <m.mosayyebi@tecteam.ir>
2023-07-02 15:15:03 +0330

    texts fixed

--- resources/views/admin/pages/inbounds/add-multiple.blade.php
+++ resources/views/admin/pages/inbounds/add-multiple.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layout.main')
 
-@section('title', __('app.pageComponents.add') .' '. __('app.inbounds.inbound'))
+@section('title', __('app.pageComponents.add') .' '. __('app.inbounds.inbounds'))
 
 @section('content')
 
@@ -26,14 +26,14 @@
                     <div class="row">
 
                         <div class="col-md-4 mb-3">
-                            <label class="form-label required">{{__('app.inbounds.title')}}</label>
+                            <label class="form-label required">{{__('app.general.title')}}</label>
                             <div>
-                                <input type="text" name="title" class="form-control" placeholder="{{__('app.inbounds.title')}}">
+                                <input type="text" name="title" class="form-control" placeholder="{{__('app.general.title')}}">
                             </div>
                         </div>
 
                         <div class="col-md-4 mb-3">
-                            <label class="form-label required">{{__('app.inbounds.ip') . ':' . __('app.inbounds.port')}}</label>
+                            <label class="form-label required">{{__('app.general.ip') . ':' . __('app.general.port')}}</label>
                             <div class="input-group input-group-flat">
                                 <input type="text" class="form-control w-75 border_right" name="ip" placeholder="192.168.1.1" autocomplete="off">
                                 <input type="text" class="form-control w-25 border_left" name="port" placeholder="443">
@@ -41,25 +41,25 @@
                         </div>
 
                         <div class="col-md-4 mb-3">
-                            <label class="form-label required">{{__('app.inbounds.date')}}</label>
+                            <label class="form-label required">{{__('app.general.date')}}</label>
                             <div>
                                 <input type="text" name="date" class="form-control datepicker"
-                                    placeholder="{{__('app.inbounds.date')}}">
+                                    placeholder="{{__('app.general.date')}}">
                             </div>
                         </div>
 
                         <div class="col-md-12 mb-3">
-                            <label class="form-label required">{{__('app.inbounds.link')}}</label>
+                            <label class="form-label required">{{__('app.general.link')}}</label>
                             <div>
-                                <input type="text" name="link" class="form-control" placeholder="{{__('app.inbounds.link')}}">
+                                <input type="text" name="link" class="form-control" placeholder="{{__('app.general.link')}}">
                             </div>
                         </div>
 
                         <div class="col-md-12">
-                            <label class="form-label">{{__('app.inbounds.description')}}</label>
+                            <label class="form-label">{{__('app.general.description')}}</label>
                             <div>
                                 <input type="text" name="description" class="form-control"
-                                    placeholder="{{__('app.inbounds.description')}}">
+                                    placeholder="{{__('app.general.description')}}">
                             </div>
                         </div>
                         
