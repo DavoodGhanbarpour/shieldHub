d8efc21dd6bb4d7bf5f86d8592bc0d5bf9713ecf
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-07-03 23:44:44 +0330

    feat: storage usage

--- resources/views/admin/pages/home/index.blade.php
+++ resources/views/admin/pages/home/index.blade.php
@@ -170,16 +170,13 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <p class="mb-3">Using Storage <strong>6854.45 MB </strong>of 8 GB</p>
+                            <p class="mb-3">Using Storage <strong> {{$system_statics['usedDisk']}} GB </strong>of {{$system_statics['totalDisk']}} GB</p>
                             <div class="progress progress-separated mb-3">
                                 <div class="progress-bar bg-primary" role="progressbar" style="width: 44%"
                                     aria-label="Regular">
                                 </div>
                                 <div class="progress-bar bg-info" role="progressbar" style="width: 19%"
                                     aria-label="System">
-                                </div>
-                                <div class="progress-bar bg-success" role="progressbar" style="width: 9%"
-                                    aria-label="Shared">
                                 </div>
                             </div>
                             <div class="row">
@@ -192,11 +189,6 @@
                                     <span class="legend me-2 bg-info"></span>
                                     <span>System</span>
                                     <span class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">415MB</span>
-                                </div>
-                                <div class="col-auto d-flex align-items-center px-2">
-                                    <span class="legend me-2 bg-success"></span>
-                                    <span>Shared</span>
-                                    <span class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">201MB</span>
                                 </div>
                                 <div class="col-auto d-flex align-items-center ps-2">
                                     <span class="legend me-2"></span>
