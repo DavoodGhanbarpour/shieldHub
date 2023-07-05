373407aceb9c95403044bb41327ea35eb893a0aa
mahdi <m.mosayyebi@tecteam.ir>
2023-07-05 23:43:27 +0330

    menu in mobile

--- resources/views/customer/layout/navbar.blade.php
+++ resources/views/customer/layout/navbar.blade.php
@@ -1,5 +1,9 @@
 <header class="navbar navbar-expand-md d-print-none" data-bs-theme="dark">
     <div class="container-xl">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <h1 class="navbar-brand navbar-brand-autodark">
             <a href="{{ route('customer.home') }}" class="text-decoration-none d-flex align-items-center">
                 <img src="{{asset('static/logo_croped.png')}}" width="110" height="32" alt="{{env('APP_NAME')}}"
@@ -8,7 +12,7 @@
             </a>
         </h1>
         <div class="navbar-nav flex-row order-md-last">
-            <div class="d-flex align-items-center">
+            <div class="d-none d-sm-flex align-items-center">
                 <div class="row">
                     <a class="w-auto px-0"
                         href="{{route('profile.locale.update', ['locale' => \App\Models\User::SUPPORTED_LANGUAGES['en']['key'], 'user' => auth()->user()->id])}}">
@@ -34,9 +38,52 @@
                         <div class="mt-1 small text-muted">{{__('app.auth.roles.'.auth()->user()->role)}}</div>
                     </div>
                 </a>
+                
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    {{-- <a href="./profile.html" class="dropdown-item">Profile</a> --}}
-                    <a href="{{ route('auth.logout') }}" class="dropdown-item">{{__('app.auth.logout')}}</a>
+                    <a href="?theme=dark" class="d-sm-none justify-content-between dropdown-item hide-theme-dark">
+                        Enable dark mode
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+                        </svg>
+                    </a>
+                    <a href="?theme=light" class="d-sm-none justify-content-between dropdown-item hide-theme-light">
+                        Enable light mode
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                            <path
+                                d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+                        </svg>
+                    </a>
+
+
+                    <a class="d-sm-none justify-content-between dropdown-item"
+                        href="{{route('profile.locale.update', ['locale' => \App\Models\User::SUPPORTED_LANGUAGES['en']['key'], 'user' => auth()->user()->id])}}">
+                        English
+                        <span class="flag btn flag-xs flag-country-gb" role="button"></span>
+                    </a>
+                    <a class="d-sm-none justify-content-between dropdown-item"
+                        href="{{route('profile.locale.update', ['locale' => \App\Models\User::SUPPORTED_LANGUAGES['fa']['key'], 'user' => auth()->user()->id])}}">
+                        فارسی
+                        <span class="flag btn flag-xs flag-country-ir" role="button"></span>
+                    </a>
+
+                    <a href="{{ route('auth.logout') }}" class="dropdown-item justify-content-between">
+                        {{__('app.auth.logout')}}
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
+                            <path d="M9 12h12l-3 -3"></path>
+                            <path d="M18 15l3 -3"></path>
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
