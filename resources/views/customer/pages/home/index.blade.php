6393bf23666867fa542e54caea86a5529bd36df2
mahdi <m.mosayyebi@tecteam.ir>
2023-07-06 11:51:45 +0330

    qr code

--- resources/views/customer/pages/home/index.blade.php
+++ resources/views/customer/pages/home/index.blade.php
@@ -269,7 +269,7 @@
                     <h5 class="modal-title">QR Code</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div id="QRCodeDisplay" class="modal-body">
+                <div id="QRCodeDisplay" class="modal-body bg-white">
 
                 </div>
                 <div class="modal-footer copy-parent">
