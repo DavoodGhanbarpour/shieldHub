163e03c5fbae4057bc4e720503f803fdd40c058c
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2023-08-06 22:08:31 +0330

    fix: bug fixed

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -2,16 +2,19 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class AuthRepository
 {
     public function authenticate(string $email, string $password, bool $remember = false): bool
     {
-        if (Auth::attempt([
+        if (Auth::attemptWhen([
             'email' => $email,
             'password' => $password,
-        ], $remember)) {
+        ],function (User $user){
+            return !$user->isDisabled();
+        } , $remember)) {
             request()->session()->regenerate();
 
             return true;
